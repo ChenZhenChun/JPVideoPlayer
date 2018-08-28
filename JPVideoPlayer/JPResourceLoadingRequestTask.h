b6a73aa3fb31c9392354a31a64184b952638fb16
gulu <346891964@qq.com>
2018-08-28 17:55:25 +0800

    Revert "回滚"
    This reverts commit 503a91789f274837290c85d9e28c3cdbc6874b5b.

--- JPVideoPlayer/JPResourceLoadingRequestTask.h
+++ JPVideoPlayer/JPResourceLoadingRequestTask.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the JPVideoPlayer package.
  * (c) NewPan <13246884282@163.com>
- *  1234567890
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
